ab54821356b33b88a93f05a0839fdfc37d2508dd
Saqib Ejaz <73016154+saqibwarriach738@users.noreply.github.com>
2022-04-25 10:19:59 +0500

    Create Assignment2.dart

---
+++ Assignment2.dart
@@ -0,0 +1,29 @@
+
+   
+main(){
+
+String myPhoneNo= "03435713097";
+int myPhoneNoInIntegar = int.parse(myPhoneNo);
+print("My Phone Number is $myPhoneNoInIntegar");
+// My Phone Number is 3435713097
+String myWight = "65";
+double myWightInDouble = double.parse(myWight);
+print("My wight is $myWightInDouble kg");
+// My wight is 65.0 kg
+ double myHeight = 5.6;
+ String myHeightInString = (myHeight.toString());
+ print("My height is $myHeightInString meter");
+//  My height is 5.6 meter
+ int myHeightInInt = myHeight.toInt();
+ print("My height is $myHeightInInt meter");
+// My height is 5 meter
+ int myAge = 24;
+ String myAgeInString = myAge.toString();
+ print("My age is $myAgeInString year");
+//  My age is 24 year
+ double myAgeInDouble = myAge.toDouble();
+ print("My age is $myAgeInDouble year");
+//  My age is 24.0 year
+
+
+}
